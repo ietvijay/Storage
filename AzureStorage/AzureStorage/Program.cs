3b1422632d0ae93eeee37d9bca5369d1d22c066f
ISONTDOM1\I96972 <vyadav@verisk.com>
2016-12-12 14:03:18 -0500

    Updated the program

--- AzureStorage/AzureStorage/Program.cs
+++ AzureStorage/AzureStorage/Program.cs
@@ -29,16 +29,69 @@ namespace BlobStorage
             Console.WriteLine("Please provide full filepath.");
             string filepath = Console.ReadLine();
 
+            //1. Upload to block blob
             if(File.Exists(filepath))
             {
                 using (var fileStream = File.OpenRead(filepath))
                 {
-
-                    var blob= container.GetBlobReference("myblob");
-
-                  
+                    var blob= container.GetBlockBlobReference("myblob");
+                    blob.UploadFromStream(fileStream);
                 }
                 
+            }
+            Console.WriteLine("Block blob uploaded successfully.");
+
+
+            //2. Listing the blobs in a container
+            Console.WriteLine("List the blob items? Y/N");
+            if (Console.ReadLine().Equals("Y", StringComparison.CurrentCultureIgnoreCase))
+            {
+
+                foreach (IListBlobItem item in container.ListBlobs(null, false))
+                {
+                    if (item.GetType() == typeof(CloudBlockBlob))
+                    {
+                        CloudBlockBlob blockblob = (CloudBlockBlob)item;
+
+                        Console.WriteLine("Block blob of length {0}:{1}", blockblob.Properties.Length, blockblob.Uri);
+                    }
+                    if (item.GetType() == typeof(CloudAppendBlob))
+                    {
+                        CloudAppendBlob appendblob = (CloudAppendBlob)item;
+
+                        Console.WriteLine("Append blob of length {0}:{1}", appendblob.Properties.Length, appendblob.Uri);
+                    }
+                    if (item.GetType() == typeof(CloudBlobDirectory))
+                    {
+                        CloudBlobDirectory directory = (CloudBlobDirectory)item;
+
+                        Console.WriteLine("Direcotry:{0}", directory.Uri);
+                    }
+                }
+            }
+
+            CloudBlockBlob cloudBlockBlob = container.GetBlockBlobReference("myblob");
+            //3. Download a blob
+            Console.WriteLine("Download a blob? Y/N");
+            if (Console.ReadLine().Equals("Y", StringComparison.CurrentCultureIgnoreCase))
+            {
+                string downloadFilePath = Console.ReadLine();
+
+                using (var fileStream = File.OpenWrite(downloadFilePath))
+                {
+                    cloudBlockBlob.DownloadToStream(fileStream);
+                }
+
+                Console.WriteLine("Download complete.");
+            }
+
+
+            //4. Delete the blob
+            Console.WriteLine("Delete the blob? Y/N");
+            if (Console.ReadLine().Equals("Y", StringComparison.CurrentCultureIgnoreCase))
+            {
+                cloudBlockBlob.Delete();
+                Console.WriteLine("Deleted successfully.");
             }
 
 
